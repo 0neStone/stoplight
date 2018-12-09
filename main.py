213e9a121d1c926f84852d6c1196a52f91a2c815
0neStone <its.johannes@yahoo.com>
2018-12-09 20:15:02 +0100

    created seperate pens for each instance

--- main.py
+++ main.py
@@ -1,37 +1,39 @@
 import turtle
 import time
 
-# Aufgabe: Ampel erstellen, die mit Bytes gesteuert wird
-
-pen = turtle.Turtle()  # Initialize pen
-pen.hideturtle()
-pen.speed(100)
+turtle.title("Johannes' Stoplight")
 
 class Stoplight:
-    def __init__(self, radius):
+    def __init__(self, radius, x, y):
         self.r = radius
+        self.x = x              # Verschiebung in y und x Richtung
+        self.y = y
         self.width = 2*self.r +20
         self.height = 3*(2*self.r) +40
 
+        self.pen = turtle.Turtle()  # Initialize pen
+        self.pen.hideturtle()
+        self.pen.speed(100)
+
     # Create black box in which all the lights fit
     def create_box(self):
-        pen.color("black", "black")
+        self.pen.color("black", "black")
 
-        pen.up()
-        pen.goto(-(self.width/2), self.height/2)
-        pen.down()
+        self.pen.up()
+        self.pen.goto(-(self.width/2) + self.x, self.height/2 + self.y)
+        self.pen.down()
 
-        pen.begin_fill()
-        pen.forward(self.width)
-        pen.right(90)
-        pen.forward(self.height)
-        pen.right(90)
-        pen.forward(self.width)
-        pen.right(90)
-        pen.forward(self.height)
-        pen.end_fill()
+        self.pen.begin_fill()
+        self.pen.forward(self.width)
+        self.pen.right(90)
+        self.pen.forward(self.height)
+        self.pen.right(90)
+        self.pen.forward(self.width)
+        self.pen.right(90)
+        self.pen.forward(self.height)
+        self.pen.end_fill()
 
-        pen.left(90)
+        self.pen.left(90)
 
     # Rotes Licht erschaffen/verändern
     def red_light(self, byte: str):
@@ -39,13 +41,13 @@ class Stoplight:
         if byte == "00000011":
             color = "red"
 
-        pen.up()
-        pen.goto(0, -self.r)
-        pen.down()
-        pen.color("grey", color)
-        pen.begin_fill()
-        pen.circle(self.r)
-        pen.end_fill()
+        self.pen.up()
+        self.pen.goto(0 + self.x, -self.r + self.y)
+        self.pen.down()
+        self.pen.color("grey", color)
+        self.pen.begin_fill()
+        self.pen.circle(self.r)
+        self.pen.end_fill()
 
     # Gelbes Licht erschaffen/verändern
     def yellow_light(self, byte: str):
@@ -53,13 +55,13 @@ class Stoplight:
         if byte == "00000010":
             color = "yellow"
 
-        pen.up()
-        pen.goto(0, self.r)
-        pen.down()
-        pen.color("grey", color)
-        pen.begin_fill()
-        pen.circle(self.r)
-        pen.end_fill()
+        self.pen.up()
+        self.pen.goto(0 + self.x, self.r + self.y)
+        self.pen.down()
+        self.pen.color("grey", color)
+        self.pen.begin_fill()
+        self.pen.circle(self.r)
+        self.pen.end_fill()
 
     # Grünes Licht erschaffen/verändern
     def green_light(self, byte: str):
@@ -67,17 +69,49 @@ class Stoplight:
         if byte == "00000001":
             color = "green"
 
-        pen.up()
-        pen.goto(0, 3*self.r)
-        pen.down()
-        pen.color("grey", color)
-        pen.begin_fill()
-        pen.circle(self.r)
-        pen.end_fill()
+        self.pen.up()
+        self.pen.goto(0 + self.x, 3*self.r + self.y)
+        self.pen.down()
+        self.pen.color("grey", color)
+        self.pen.begin_fill()
+        self.pen.circle(self.r)
+        self.pen.end_fill()
 
     # Lichter zum ersten Mal erstellen
     def create_lights(self):
-        self.create_box()
+        self.red_light("00000011")
+
+    # Lichter durch Byte-Befehle verändern
+    def change_lights(self, byte: str):
+        self.green_light(byte)
+        self.yellow_light(byte)
+        self.red_light(byte)
+
+    def run_lights(self, repeat: int):
+        time.sleep(2)
+        for i in range(repeat):
+            # Falls Benutzer das Programm schließt
+            try:
+                self.change_lights("00000001") # green light
+                time.sleep(2)
+                self.change_lights("00000010") # yellow light
+                time.sleep(2)
+                self.change_lights("00000011") # red light
+                time.sleep(2)
+                self.change_lights("00000010") # yellow light
+                time.sleep(2)
+            except:
+                pass
+
+Light1 = Stoplight(80, -200, 0)
+Light1.create_lights()
+Light2 = Stoplight(80, 200, 0)
+Light2.create_lights()
+
+Light1.run_lights(2)
+Light2.run_lights(2)
+
+turtle.mainloop()    self.create_box()
         self.green_light("00000001")
         self.yellow_light("00000010")
         self.red_light("00000011")
@@ -88,14 +122,28 @@ class Stoplight:
         self.yellow_light(byte)
         self.red_light(byte)
 
-Light1 = Stoplight(80)
+    def run_lights(self, repeat: int):
+        time.sleep(2)
+        for i in range(repeat):
+            # Falls Benutzer das Programm schließt
+            try:
+                self.change_lights("00000001") # green light
+                time.sleep(2)
+                self.change_lights("00000010") # yellow light
+                time.sleep(2)
+                self.change_lights("00000011") # red light
+                time.sleep(2)
+                self.change_lights("00000010") # yellow light
+                time.sleep(2)
+            except:
+                pass
 
+Light1 = Stoplight(80, -200, 0)
 Light1.create_lights()
-time.sleep(2)
-Light1.change_lights("00000001") # green light
-time.sleep(2)
-Light1.change_lights("00000010") # yellow light
-time.sleep(2)
-Light1.change_lights("00000011") # red light
+Light2 = Stoplight(80, 200, 0)
+Light2.create_lights()
+
+Light1.run_lights(2)
+Light2.run_lights(2)
 
 turtle.mainloop()
